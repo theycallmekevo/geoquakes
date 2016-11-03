c615f903b8a32d8111744dba8cced17ab1f29ca2
Kevin Tran <kevin2005tran@gmail.com>
2016-11-02 18:36:37 -0700

    finished geoquakes assignment

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,44 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).on("ready", function() {
+//   var source = $('#earthquake-li-templete').html();
+//   console.log('template script source:', source);
+//
+//   var template = Handlebars.compile(source);
+//
+//   var developerHtml = template({ earthquakes: data.features });
+//   console.log('generated html string:', developerHtml);
+//
+//   $("#earthquake-list").append(developerHtml);
 
-  // CODE IN HERE!
+function getQuakes(){
+  $.ajax({
+    method: 'GET',
+    url: weekly_quakes_endpoint,
+    dataType: 'JSON',
+    success: onSuccess
+  });
+}
+function onSuccess(json){
+  var source = $('#earthquake-li-templete').html();
+  var template = Handlebars.compile(source);
+  var quakesHtml = template({earthquakes: json.features})
+  $('#earthquake-list').append(quakesHtml);
+  dropPins(json);
+}
 
+  function dropPins(json) {
+    console.log('dropPins works');
+  for (var i = 0; i < json.features.length; i++) {
+    console.log('right coordinates');
+  var coords = json.features[i].geometry.coordinates;
+  var latLng = new google.maps.LatLng(coords[1],coords[0]);
+  var marker = new google.maps.Marker({
+   position: latLng,
+   map: map
+  });
+  }
+  }
+
+getQuakes();
 });
---
+++ scripts/data.js
@@ -0,0 +1,173 @@
+var data = {
+    type: "FeatureCollection",
+    metadata: {
+        generated: 1478111185000,
+        url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
+        title: "USGS Significant Earthquakes, Past Week",
+        status: 200,
+        api: "1.5.2",
+        count: 4
+    },
+    features: [{
+        type: "Feature",
+        properties: {
+            mag: 4.5,
+            place: "14km ESE of Pawnee, Oklahoma",
+            time: 1478060814200,
+            updated: 1478110977246,
+            tz: -300,
+            url: "http://earthquake.usgs.gov/earthquakes/eventpage/us100073m6",
+            detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us100073m6.geojson",
+            felt: 2951,
+            cdi: 5.7,
+            mmi: 4.82,
+            alert: "green",
+            status: "reviewed",
+            tsunami: 0,
+            sig: 882,
+            net: "us",
+            code: "100073m6",
+            ids: ",us100073m6,",
+            sources: ",us,",
+            types: ",cap,dyfi,general-link,geoserve,losspager,moment-tensor,origin,phase-data,shakemap,",
+            nst: null,
+            dmin: 0.136,
+            rms: 0.62,
+            gap: 65,
+            magType: "mwr",
+            type: "earthquake",
+            title: "M 4.5 - 14km ESE of Pawnee, Oklahoma"
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [-96.6472,
+                36.3077,
+                2.56
+            ]
+        },
+        id: "us100073m6"
+    }, {
+        type: "Feature",
+        properties: {
+            mag: 5.4,
+            place: "40km E of Colombia, Colombia",
+            time: 1477873212590,
+            updated: 1477971299859,
+            tz: -300,
+            url: "http://earthquake.usgs.gov/earthquakes/eventpage/us1000735j",
+            detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us1000735j.geojson",
+            felt: 374,
+            cdi: 4.8,
+            mmi: null,
+            alert: null,
+            status: "reviewed",
+            tsunami: 0,
+            sig: 628,
+            net: "us",
+            code: "1000735j",
+            ids: ",us1000735j,gcmt20161031002012,",
+            sources: ",us,gcmt,",
+            types: ",cap,dyfi,geoserve,impact-text,moment-tensor,origin,phase-data,",
+            nst: null,
+            dmin: 1.403,
+            rms: 0.78,
+            gap: 46,
+            magType: "mb",
+            type: "earthquake",
+            title: "M 5.4 - 40km E of Colombia, Colombia"
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [-74.4445,
+                3.4372,
+                44
+            ]
+        },
+        id: "us1000735j"
+    }, {
+        type: "Feature",
+        properties: {
+            mag: 6.6,
+            place: "6km N of Norcia, Italy",
+            time: 1477809619200,
+            updated: 1478104122158,
+            tz: 120,
+            url: "http://earthquake.usgs.gov/earthquakes/eventpage/us1000731j",
+            detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us1000731j.geojson",
+            felt: 1123,
+            cdi: 8.5,
+            mmi: 8.56,
+            alert: "red",
+            status: "reviewed",
+            tsunami: 0,
+            sig: 2850,
+            net: "us",
+            code: "1000731j",
+            ids: ",us1000731j,gcmt20161030064019,",
+            sources: ",us,gcmt,",
+            types: ",cap,dyfi,general-link,general-text,geoserve,losspager,moment-tensor,origin,phase-data,shakemap,",
+            nst: null,
+            dmin: 0.181,
+            rms: 1.16,
+            gap: 28,
+            magType: "mww",
+            type: "earthquake",
+            title: "M 6.6 - 6km N of Norcia, Italy"
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [
+                13.0884,
+                42.8547,
+                10
+            ]
+        },
+        id: "us1000731j"
+    }, {
+        type: "Feature",
+        properties: {
+            mag: 6.1,
+            place: "3km W of Visso, Italy",
+            time: 1477509488320,
+            updated: 1478022073766,
+            tz: 120,
+            url: "http://earthquake.usgs.gov/earthquakes/eventpage/us1000725y",
+            detail: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us1000725y.geojson",
+            felt: 516,
+            cdi: 7.5,
+            mmi: 8.55,
+            alert: "red",
+            status: "reviewed",
+            tsunami: 0,
+            sig: 2387,
+            net: "us",
+            code: "1000725y",
+            ids: ",us1000725y,gcmt20161026191808,",
+            sources: ",us,gcmt,",
+            types: ",cap,dyfi,general-link,general-text,geoserve,impact-text,losspager,moment-tensor,origin,phase-data,shakemap,",
+            nst: null,
+            dmin: 0.249,
+            rms: 1.24,
+            gap: 21,
+            magType: "mww",
+            type: "earthquake",
+            title: "M 6.1 - 3km W of Visso, Italy"
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [
+                13.0434,
+                42.9343,
+                10
+            ]
+        },
+        id: "us1000725y"
+    }],
+    bbox: [-96.6472,
+        3.4372,
+        2.56,
+        13.0884,
+        42.9343,
+        44
+    ]
+}
